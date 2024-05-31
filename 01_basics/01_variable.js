79c2e4b41215305ba04de88043829e4fc49eb4b2
VARUN KUMAR <varunkumar.14780@gmail.com>
2024-05-31 18:29:55 +0530

    variable declarations.

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,1 +1,11 @@
 console.log('Varun Pandey');
+const accountId = 147801
+let accountEmail = 'varun@gmail.com'
+var accountPassword = '1667491'
+accountCity = 'Gaya'  // accepter in js., maybe in form of let.
+let accountState;  // if not initialize then, result in 'undefined'.
+
+/*
+  var: it has a issue in block and functional scope.
+*/
+console.table([accountId, accountEmail, accountPassword,accountCity, accountState]);
