c2514c816396cd71427beb9978bb0d4ae02393d3
VARUN KUMAR <varunkumar.14780@gmail.com>
2024-05-31 21:48:02 +0530

    all about the different data-types in js.

---
+++ 01_datatype.js
@@ -0,0 +1,16 @@
+const num = 6516842
+let str ='varun pandey'
+let isvalid = true
+
+/*
+  number
+  bigint
+  string
+  boolean
+  null - object
+  undefined - undefined
+  symbol - unique
+  object
+
+
+*/
